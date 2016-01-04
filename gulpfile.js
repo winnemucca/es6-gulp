2a50b4a4c38bb229166c7b04da182074d67ecefb
winnemucca <skauyedauty@gmail.com>
2016-01-04 10:48:42 -0700

    fixed some updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
-
 var browserSync = require('browser-sync');
-
-gulp.task("default", function () {
+var clean = require('gulp-clean');
+// change default
+gulp.task("babelIt", function () {
 	console.log('in');
-  return gulp.src("src/app.js")
+  return gulp.src("src/**/*.js")
     .pipe(babel())
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task("copyIndex", function() {
+
+gulp.task("copyIndex", ['clean'], function() {
 	gulp.src("src/index.html")
 	.pipe(gulp.dest('./build'))
 	.pipe(browserSync.reload({stream: true}));
@@ -26,5 +28,14 @@ gulp.task("browserSync", function() {
 });
 
 gulp.task('watchFiles', function() {
-	gulp.watch('src/index.html', ['copyIndex'])
+	gulp.watch('src/index.html', ['copyIndex']);
+	gulp.watch('src/**/*.js', ['babelIt']);
 })
+
+gulp.task('clean', function() {
+	return gulp.src('./build', {read: false})
+			.pipe(clean());
+});
+
+
+gulp.task('default', ['copyIndex', 'browserSync', 'watchFiles']);
---
+++ src/person.js
@@ -0,0 +1,11 @@
+class Person
+{
+	constructor(firstName) 
+	{
+		this.firstName = firstName;
+	}
+	sayName()
+	{
+		console.log('sup ', this.firstName);
+	}
+}
